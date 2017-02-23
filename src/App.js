828ae7bd639b472be96def644afc396ac7bf274e
Kathy Doody <kathy.doody@trinetx.com>
2017-02-23 08:20:09 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Preloader from './components/Preloader';
+
+class App extends Component {
+  state = {
+    techSalaries: []
+  }
+
+  render() {
+    if (this.state.techSalaries.length < 1) {
+        return (
+          <Preloader />
+        );
+    }
+    return (
+        <div className="App">
+
+        </div>
+    );
+  }
+
+}
+export default App;
